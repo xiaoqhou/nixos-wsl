a3b1876377a4552c1d510157abfce6f405d09a94
xiaoqhou <houxq.bj@outlook.com>
2026-05-02 23:06:34 +0200

    add hermes agent

--- home/env/hermes-agent.nix
+++ home/env/hermes-agent.nix
@@ -1,26 +1,41 @@
-{pkgs, config, ...}: {
-  home.packages = with pkgs; [
-    pre-commit
-    kubectl
-    kubectx
-    go
-    gh
-    opencode
-    # dependencies of hermes agent
-    uv
-    ffmpeg
-    playwright
-#    nodejs_24
-    python311
-    python3Packages.pip
-  ];
+{
+  pkgs,
+  config,
+  ...
+}: {
+  home.packages = with pkgs;
+    [
+      pre-commit
+      kubectl
+      kubectx
+      go
+      gh
+      opencode
+      # dependencies of hermes agent
+      uv
+      ffmpeg
+      playwright # required by agent-browser
+      chromium # required by agent-browser
+      # nodejs_24
+      (python311.withPackages (ps:
+        with ps; [
+          pip
+          # the following are used by llm-wiki skill
+          pypdf
+          python-docx
+        ]))
+      binutils # include strings
+    ]
+    ++ (with pkgs.unstable; [
+      agent-browser
+    ]);
 
   programs.bash = {
     # enable = true;
     bashrcExtra = ''
-        if [ -f ~/.gh_token ]; then
-            export GH_TOKEN=$(cat ~/.gh_token) #used by gh and hermes
-        fi
+      if [ -f ~/.gh_token ]; then
+          export GH_TOKEN=$(cat ~/.gh_token) #used by gh and hermes
+      fi
     '';
   };
 
@@ -28,16 +43,23 @@
     "${config.home.homeDirectory}/.local/bin"
   ];
 
-  #  link the driver d
-  home.file = {
-    # ".hermes" = {
-    #   source = config.lib.file.mkOutOfStoreSymlink "/mnt/d/agent/hermes";
-    #   recursive = true; 
-    # };
-    "workspace" = {
-      source = config.lib.file.mkOutOfStoreSymlink "/mnt/d/agent/workspace";
-      recursive = true; 
-    };
+  home.shellAliases = {
+    her = "hermes";
+    herui = "~/hermes-webui/start.sh";
   };
 
+  #  link the driver d
+  home.file = {
+    "hermes-webui" = {
+      source = builtins.fetchGit {
+        url = "https://github.com/nesquena/hermes-webui";
+        rev = "219f5d6ce5f04eb4121b3893decb92ab79e48337"; #v0.50.253
+      };
+      recursive = true;
+    };
+    "workspace" = {
+      source = config.lib.file.mkOutOfStoreSymlink "/mnt/d/agent/workspace";
+      recursive = true;
+    };
+  };
 }
